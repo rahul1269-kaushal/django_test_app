645f4190fd31fa96d34ba8d4c7019e454102c584
rkaushal <rkaushal@digitalocean.com>
2024-01-30 20:58:28 +0530

    render templates

--- movies/views.py
+++ movies/views.py
@@ -2,9 +2,10 @@
 from django.http import HttpResponse, request
 from django.shortcuts import render
 
+data = {'movies': ['MADAR','CHOR1']}
 
 def movies(request):
-    return HttpResponse("Hello World")
+    return render(request, "hello.html", data )
 
 def home(request):
     return HttpResponse("Homepage")
